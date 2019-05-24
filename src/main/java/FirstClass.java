3cff8f7dd9f0243fc25db8c083258987fb65b7c7
Jun <junjiexavier@hotmail.com>
2019-05-25 02:45:23 +0800

    commit n push

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,22 @@
+public class FirstClass {
+  int x = 2;
+  double money = 4.99;
+  char myc = 'c';
+
+
+
+  public static void main(String[] args) {
+    int x = 2;
+    double money = 4.99;
+    char myc = 'c';
+    boolean truth = true;
+    boolean lie = false;
+    String daily_report = "We have " + x + " We also have " + (money*x) + " we dun have " + myc;
+    daily_report += " and hihi is u\n";
+    System.out.println("Hey there");
+    System.out.println(daily_report);
+    System.out.println(truth);
+    System.out.println(lie);
+}
+
+}
---
+++ src/main/java/Lesson2.java
@@ -0,0 +1,18 @@
+public class Lesson2 {
+
+  public static void main(String[] args) {
+    double numbera = 2;
+    double numberb = 5;
+    double total = numbera+numberb;
+    double power_result;
+    power_result =  Math.pow(numberb,numbera);
+
+    System.out.println(power_result);
+    System.out.println(numbera / numberb);
+    int a =5;
+    double b = 6.0;
+    int products = a*(int)b;
+    //number casting
+    System.out.println(products);
+  }
+}
